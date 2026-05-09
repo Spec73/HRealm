e1317f491ef80454678f8b8f67d8279166043c37
Spec73 <sip.exiazaryan55@gmail.com>
2026-05-10 03:49:45 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 const DB_FILE = 'users.json';
 if (!fs.existsSync(DB_FILE)) {
